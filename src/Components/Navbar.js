623e072afdc72335ab8cef49c1f9ea77e8c2320f
Mr-Edd834 <kagorieddy76@gmail.com>
2025-10-31 01:15:27 +0300

    added fav icon

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,8 +4,8 @@ import './Navbar.css'
 const Navbar = () => {
   return (
     <div className="navbar">
-        <img className="logo" src="/gogrub-logo.png" alt="logo" />
-        <img className="profile" src="/IMG-20250815-WA0006.jpg" alt="profile pic" />
+        <img className="logo" src="/govendor-logo (1).png" alt="logo" />
+        <img className="profile" src="gogrub-logo.png" alt="profile pic" />
 
     </div>
   )
